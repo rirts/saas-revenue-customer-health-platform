9ad499e932536b9d5199de5482cc9311b041e991
rirts <rafartejadasegura@gmail.com>
2026-05-26 16:27:08 -0600

    Add local Postgres and Metabase infrastructure

---
+++ docker/postgres/init/01_create_schemas.sql
@@ -0,0 +1,5 @@
+create schema if not exists raw;
+create schema if not exists staging;
+create schema if not exists intermediate;
+create schema if not exists marts;
+create schema if not exists monitoring;
